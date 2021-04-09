6627b623843d653e52cbffe817123727e52c2a36
Teodes <jgrachot@gmail.com>
2021-04-08 22:10:25 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
