480fd460be5517d4d94a422bad9f56e666180b53
Teodes <jgrachot@gmail.com>
2021-04-11 18:33:34 -0300

    Creacion de Proyecto

---
+++ Cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatGenerator
+{
+    class Cat
+    {
+        static int color, fondo, contador;
+        public Cat()
+        {
+        }
+        static public void CrearGato()
+        {
+            color = 0;
+
+
+
+
+
+
+
+            Console.WriteLine("       _                         \n" +
+                              "       \\`*-.                    \n" +
+                              "        )  _`-.                  \n" +
+                              "       .  : `. .                 \n" +
+                              "       : _   '  \\               \n" +
+                              "       ; *` _.   `*-._           \n" +
+                              "       `-.- '          `-.       \n" +
+                              "         ;       `       `.      \n" +
+                              "         :.       .        \\    \n" +
+                              "         . \\  .   :   .-'   .   \n" +
+                              "         '  `+.;  ;  '      :    \n" +
+                              "         :  '  |    ;       ;-.  \n" +
+                              "         ; '   : :`-:     _.`* ; \n" +
+                              "     .* ' /  .*'; .*`-+'  `*'    \n" +
+                              "      `*-*   `*-*  `*-*'         \n");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace CatGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola, Bienvenido al programa de creación de gatos!!");
+
+            do
+            {
+                Cat.CrearGato();
+            } while (Continue());
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine("¿Desea crear otro gato? [Y / N]\n");
+            var answer = Console.ReadKey(true);
+            while (answer.Key != ConsoleKey.N && answer.Key != ConsoleKey.Y)
+            {
+                Console.WriteLine("La tecla presionada no es valida, por favor intente nuevamente.\t [Y / N]\n");
+                answer = Console.ReadKey(true);
+            }
+            if (answer.Key == ConsoleKey.Y)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.Black;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
